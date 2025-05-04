e0332b6728bea728ea94850364316449e404ed5a
Manas verma <manasverma1357@gmail.com>
2025-05-04 18:37:22 +0530

    Select DOM elements to manipulate

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const productList = document.getElementById("product-list");
+    const cartItems = document.getElementById("cart-items");
+    const emptyCartMsg = document.getElementById("empty-cart");
+    const totalPrice = document.getElementById("total-price");
+    const cartTotalMsg = document.getElementById("cart-total");
+    const checkoutBtn = document.getElementById("checkout-btn");
+
+    const products = [
+        {id: 1, name: "Product 1", price: 16.99},
+        {id: 2, name: "Product 2", price: 55.99},
+        {id: 3, name: "Product 3", price: 69.999},
+        {id: 4, name: "Product 4", price: 70.00}
+    ];
+
+    const cart = [];
+})
